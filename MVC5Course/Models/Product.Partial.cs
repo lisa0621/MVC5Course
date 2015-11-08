36edb065d35b167b20378abaed9315c925c15a87
SHA-PAI LI <lisa0621@gmail.com>
2015-11-08 17:12:40 +0800

    20 自訂 DataTypeAttribute 驗證屬性「限制欄位值必須出現兩個數字1Attribute」並套用在 Product 實體的 ProductName 屬性中

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -15,6 +15,7 @@ namespace MVC5Course.Models
         public int ProductId { get; set; }
         
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [限制欄位值必須出現兩個數字1Attribute]
         public string ProductName { get; set; }
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
