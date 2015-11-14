76e71e8d2129fc4e74c92ee5716f23677ce7c833
SHA-PAI LI <lisa0621@gmail.com>
2015-11-14 14:38:19 +0800

    create BaseController

---
+++ MVC5Course/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected FabricsEntities db = new FabricsEntities();
+    }
+}
