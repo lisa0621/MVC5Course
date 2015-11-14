5e6282e0e40eef84d1079ef24c2672a37e9bd965
SHA-PAI LI <lisa0621@gmail.com>
2015-11-14 15:42:03 +0800

    練習 ViewResult, PartialViewResult, ContentResult, FileResult 的使用

--- MVC5Course/Controllers/BaseController.cs
+++ MVC5Course/Controllers/BaseController.cs
@@ -7,8 +7,34 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class BaseController : Controller
+    public abstract class BaseController : Controller
     {
         protected FabricsEntities db = new FabricsEntities();
+
+        public ActionResult Debug()
+        {
+            return View();
+            //return new ViewResult() { ViewName = "About" };
+        }
+
+
+        /// <summary>
+        /// 找不到頁面－連回首頁
+        /// </summary>
+        /// <param name="actionName"></param>
+        protected override void HandleUnknownAction(string actionName)
+        {
+            if (Request.IsLocal)
+            {
+                //不是很好的作法
+                //this.View(actionName).ExecuteResult(this.ControllerContext);
+                //this.Redirect("/").ExecuteResult(this.ControllerContext);
+                this.Redirect("/?unknow-action="+actionName).ExecuteResult(this.ControllerContext);
+            }
+            else
+            {
+                base.HandleUnknownAction(actionName);
+            }
+        }
     }
 }
---
+++ MVC5Course/Models/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class MyClass
+    {
+        public int MyProperty { get; set; }
+
+        private int my_Name;
+
+        ConsoleColor myColor = ConsoleColor.Black;
+    }
+}
