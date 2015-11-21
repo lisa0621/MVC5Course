ca28340e62a993f24f34fb9b67d54ae492c6b3e1
SHA-PAI LI <lisa0621@gmail.com>
2015-11-21 17:10:15 +0800

    ImageUrl

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -27,12 +27,14 @@ namespace MVC5Course.Models
         [AllowHtml]
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         //[限制欄位值必須出現兩個數字1Attribute]
+        [DataType(DataType.ImageUrl)]
         public string ProductName { get; set; }
         public Nullable<decimal> Price { get; set; }
 
         [UIHint("BooleanActive")]
         public Nullable<bool> Active { get; set; }
         [DisplayName("庫存數量")]
+        
         public Nullable<decimal> Stock { get; set; }
     
         public virtual ICollection<OrderLine> OrderLine { get; set; }
