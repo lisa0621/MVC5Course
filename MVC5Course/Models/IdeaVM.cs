524baeca428ccc75a6befce75c523147e39356e9
SHA-PAI LI <lisa0621@gmail.com>
2015-11-28 17:06:06 +0800

    Enum Templates

---
+++ MVC5Course/Models/IdeaVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public enum IdeaType
+    {
+        弱爆了,
+        還好,
+        歐
+    }
+
+    public class IdeaVM
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public IdeaType IdeaType { get; set; }
+    }
+}
