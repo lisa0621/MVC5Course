bc5894de1b856a4d6e043c02c593273f1b691232
SHA-PAI LI <lisa0621@gmail.com>
2015-11-28 11:28:13 +0800

    51 練習將 /OrderLines/Index 底下的 Delete 功能改成用 Ajax 的方式完成

--- MVC5Course/Controllers/OrderLinesController.cs
+++ MVC5Course/Controllers/OrderLinesController.cs
@@ -28,14 +28,15 @@ namespace MVC5Course.Controllers
                        group p by p.Order.OrderStatus into g
                        select g.Key;
 
-            ViewBag.OrderStatus = new SelectList(plist);
+            ViewBag.OrderStatus = new SelectList(list);
+            ViewBag.OrderStatusSelected = OrderStatus;
 
             if (!String.IsNullOrEmpty(OrderStatus))
             {
                 orderLine = orderLine.Where(x => x.Order.OrderStatus == OrderStatus);
             }
 
-            return PartialView(orderLine.ToList());
+            return PartialView("Index", orderLine.ToList());
         }
 
         // GET: OrderLines/Details/5
@@ -115,30 +116,36 @@ namespace MVC5Course.Controllers
             return View(orderLine);
         }
 
-        // GET: OrderLines/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            OrderLine orderLine = db.OrderLine.Find(id);
-            if (orderLine == null)
-            {
-                return HttpNotFound();
-            }
-            return View(orderLine);
-        }
-
         // POST: OrderLines/Delete/5
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        //[ValidateAntiForgeryToken]
+        public ActionResult Delete(int OrderId, int LineNumber, string OrderStatus)
         {
-            OrderLine orderLine = db.OrderLine.Find(id);
-            db.OrderLine.Remove(orderLine);
+            OrderLine ol = db.OrderLine.Find(OrderId, LineNumber);
+            db.OrderLine.Remove(ol);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            //return RedirectToAction("Index");
+
+            var productId = ol.ProductId;
+            ViewBag.productId = ol.ProductId;
+
+            //var plist = orderRepo.All().GroupBy(x => x.OrderStatus).Select(g => g.Key);
+            var plist = db.OrderLine.GroupBy(x => x.Order.OrderStatus).Select(g => g.Key);
+            var orderLine = db.OrderLine.Where(x => x.ProductId == productId);
+
+            var list = from p in orderLine
+                       group p by p.Order.OrderStatus into g
+                       select g.Key;
+
+            ViewBag.OrderStatus = new SelectList(plist);
+            ViewBag.OrderStatusSelected = OrderStatus;
+
+            if (!String.IsNullOrEmpty(OrderStatus))
+            {
+                orderLine = orderLine.Where(x => x.Order.OrderStatus == OrderStatus);
+            }
+
+            return PartialView("Index", orderLine.ToList());
         }
 
         protected override void Dispose(bool disposing)
