cf0770c73f5955abebc942588c1fe556fa982813
SHA-PAI LI <lisa0621@gmail.com>
2015-11-28 09:33:39 +0800

    49 練習使用 @Ajax.ActionLink 完成基本 Ajax 操作 ( 記得安裝必要的 NuGet 套件並將 JS 載入網頁中 )

---
+++ MVC5Course/Controllers/AjaxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String GetTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH::mm:ss:ffff");
+        }
+    }
+}
